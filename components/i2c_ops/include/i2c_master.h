2b8938758d322a243f20799a889bcc1d83123ce8
Jayanth Balan <jayanthsb.005@gmail.com>
2026-03-12 17:18:49 +0530

    device driver header and cmake changes

---
+++ components/i2c_ops/include/i2c_master.h
@@ -0,0 +1,37 @@
+
+#ifndef I2C_MASTER_H
+#define I2C_MASTER_H
+
+//Libraries
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include "esp_err.h"
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/portmacro.h"
+
+//Global Macros
+#define I2C_MASTER_CLK_STRETCH_TIMEOUT 2000U
+#define I2C_MASTER_BUS_CLR_PULSES 9U
+
+//Driver Handle
+typedef struct {
+    gpio_num_t sda;
+    gpio_num_t scl;
+    uint32_t half_period_cy;
+    uint32_t quarter_period_cy;
+    portMUX_TYPE spinlock;
+    bool initialised;
+} i2c_bb_t;
+
+//API
+esp_err_t i2c_master_init(i2c_bb_t*, gpio_num_t, gpio_num_t);
+esp_err_t i2c_master_deinit(i2c_bb_t*);
+esp_err_t i2c_master_wr(i2c_bb_t*, uint8_t, const uint8_t*, size_t, bool);
+esp_err_t i2c_master_rd(i2c_bb_t*, uint8_t, uint8_t*, size_t, bool);
+esp_err_t i2c_master_wr_rd(i2c_bb_t*, uint8_t, const uint8_t*, size_t, uint8_t*, size_t);
+bool i2c_master_bus_check(i2c_bb_t*);
+esp_err_t i2c_master_bus_recover(i2c_bb_t*);
+
+#endif
